7aeda4601ffefdab4d1b7ca249de1576d02a8c19
BrunuNC <brununc@gmail.com>
2023-12-29 16:58:46 -0400

    update

---
+++ Condicional05/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+
+namespace Condicional05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine("CODIGO\t\tESPECIFICAÇÃO\t\tPREÇO");
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine("1\t\tCachorro Quente\t\tR$ 4.00");
+            Console.WriteLine("2\t\tX-Salada\t\tR$ 4.50");
+            Console.WriteLine("3\t\tX-Bacon\t\t\tR$ 5.00");
+            Console.WriteLine("4\t\tTorrada Simples\t\tR$ 2.00");
+            Console.WriteLine("5\t\tRefrigerante\t\tR$ 1.50");
+            Console.WriteLine("---------------------------------------------------");
+
+            string[] valores = Console.ReadLine().Split(' ');
+            int codigo = int.Parse(valores[0]);
+            int quantidade = int.Parse(valores[1]);
+
+            double total;
+            if (codigo == 1)
+            {
+                total = quantidade * 4.0;
+            }
+            else if (codigo == 2)
+            {
+                total = quantidade * 4.5;
+            }
+            else if (codigo == 3)
+            {
+                total = quantidade * 5.0;
+            }
+            else if (codigo == 4)
+            {
+                total = quantidade * 2.0;
+            }
+            else
+            {
+                total = quantidade * 1.5;
+            }
+
+            Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Condicional06/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Globalization;
+
+namespace Condicional06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            double valor = double.Parse(Console.ReadLine());
+
+            if (valor >= 0.0 && valor < 25.0)
+            {
+                Console.WriteLine("INTERVALO [0,25]");
+            }
+            else if (valor >= 25.0 && valor < 50.0)
+            {
+                Console.WriteLine("INTERVALO [25,50]");
+            }
+            else if (valor >= 50.0 && valor < 75.0)
+            {
+                Console.WriteLine("INTERVALO [50,75]");
+            }
+            else if (valor >= 75.0 && valor <= 100.0)
+            {
+                Console.WriteLine("INTERVALO [75,100]");
+            }
+            else
+            {
+                Console.WriteLine("Fora de intervalo");
+            }
+            */
+
+            double numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (numero < 0.0 || numero > 100.0)
+            {
+                Console.WriteLine("Fora de intervalo");
+            }
+            else if (numero <= 25.0)
+            {
+                Console.WriteLine("Intervalo [0,25]");
+            }
+            else if (numero <= 50.0)
+            {
+                Console.WriteLine("Intervalo (25,50]");
+            }
+            else if (numero <= 75.0)
+            {
+                Console.WriteLine("Intervalo (50,75]");
+            }
+            else
+            {
+                Console.WriteLine("Intervalo (75,100]");
+            }
+        }
+    }
+}
